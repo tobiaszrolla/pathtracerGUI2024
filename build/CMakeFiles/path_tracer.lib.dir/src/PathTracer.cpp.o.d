bdbe267f19d87dfa601a4ce245e5dbd753ff53c1
tobiaszrolla <154612005+tobiaszrolla@users.noreply.github.com>
2024-11-16 20:44:26 +0100

    Delete build directory

--- build/CMakeFiles/path_tracer.lib.dir/src/PathTracer.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/path_tracer.lib.dir/src/PathTracer.cpp.o: \
- /home/tobiasz/pathtracerGUI2024/src/PathTracer.cpp \
- /usr/include/stdc-predef.h \
- /home/tobiasz/pathtracerGUI2024/src/../include/PathTracer.hpp
